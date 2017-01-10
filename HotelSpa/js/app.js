f0ab162fe50deb4ddb9c00b5ff73621ce6ba684c
RDNyel <al12542062@unadmexico.mx>
2017-01-10 09:34:55 -0600

    :+1:

--- HotelSpa/js/app.js
+++ HotelSpa/js/app.js
@@ -1,1 +1,26 @@
+function obtenerAlto() {
+    var alto = $('header').height();
+    $('#overlay').css({'height': alto+'px'});
+}
+
+(function($) {
+    
+    window.setTimeout('obtenerAlto()', 200);
+
+
+    $('#galeria .column img').wrap('<a data-open="modalGaleria">');
+
+    $('#galeria a').on('click', function() {
+        var imagen = $(this).find('img').attr('src');
+        var imagenSeleccionada = imagen.split('thumbnail');
+        var URLCompleta = imagenSeleccionada[0]+ 'fullsize' + imagenSeleccionada[1];
+        $('#imagenContenedor').attr('src', URLCompleta);
+});
+})(jQuery);
+
+$(window).resize(function() {
+    obtenerAlto();
+
+});
+
 $(document).foundation();
